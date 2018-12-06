af033301dd719667a3031441ffd04d2370ac74f0
Dezz Meister <dezzy3359@gmail.com>
2018-12-05 23:55:47 -0500

    disabled maven and deleted junk

--- Mechanical Trash/src/com/dezzy/trash/Main.java
+++ Mechanical Trash/src/com/dezzy/trash/Main.java
@@ -1,7 +1,5 @@
 package com.dezzy.trash;
 
-import org.matheclipse.core.eval.EvalUtilities;
-import org.matheclipse.core.interfaces.IExpr;
 
 public class Main {
 	
@@ -10,10 +8,6 @@ public class Main {
 		Function function = new Function('x', 'y', "y = 4xy + 1 + (x - 3)(y + 4)");
 		System.out.println(function);
 		*/
-		EvalUtilities util = new EvalUtilities();
-		IExpr result;
 		
-		result = util.evaluate("D(ArcTan(x,y),x)");
-		System.out.println(result);
 	}
 }
