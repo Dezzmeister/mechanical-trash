0c2a910a7094be96363987c8181e54e42deaef6e
Dezz Meister <dezzy3359@gmail.com>
2018-12-05 23:40:15 -0500

    no canada geese

--- Mechanical Trash/src/com/dezzy/trash/Main.java
+++ Mechanical Trash/src/com/dezzy/trash/Main.java
@@ -1,11 +1,19 @@
 package com.dezzy.trash;
 
-import com.dezzy.trash.core.Function;
+import org.matheclipse.core.eval.EvalUtilities;
+import org.matheclipse.core.interfaces.IExpr;
 
 public class Main {
 	
 	public static void main(String[] args) {
+		/*
 		Function function = new Function('x', 'y', "y = 4xy + 1 + (x - 3)(y + 4)");
 		System.out.println(function);
+		*/
+		EvalUtilities util = new EvalUtilities();
+		IExpr result;
+		
+		result = util.evaluate("D(ArcTan(x,y),x)");
+		System.out.println(result);
 	}
 }
